4de0eec1b460200a007274d5572935fc6af977f2
KameshwaranM <kameshwaran1209@gmail.com>
2024-05-21 15:33:59 +0530

    new

---
+++ src/All-Routes/Route.js
@@ -0,0 +1,17 @@
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+
+function Rout() {
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/contact" element={<Contact />} />
+        </Routes>
+      </Router>
+    </>
+  );
+}
+
+export default Rout;
