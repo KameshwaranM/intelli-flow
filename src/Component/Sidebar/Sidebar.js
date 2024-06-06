530c3f77cd82dba2ee8a3e6778284675042879ff
KameshwaranM <kameshwaran1209@gmail.com>
2024-06-06 12:47:52 +0530

    new

--- src/Component/Sidebar/Sidebar.js
+++ src/Component/Sidebar/Sidebar.js
@@ -1,5 +1,5 @@
 import React, { useState, useContext, useEffect } from "react";
-import { useLocation, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 import {
   Drawer,
   List,
